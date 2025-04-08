2e33f24167f5165be5eba16704f4f7a07c7fdb16
agatamilia <amiliaagata@student.telkomuniversity.ac.id>
2025-04-09 02:57:11 +0700

    kok error?

--- fe_chatbot/lib/config/api_config.dart
+++ fe_chatbot/lib/config/api_config.dart
@@ -7,7 +7,7 @@ class ApiConfig {
   static const bool isProduction = false;
   
   // Ngrok Configuration
-  static const String ngrokSubdomain = '7d55-2404-c0-2570-00-1701-2fef';
+  static const String ngrokSubdomain = '0871-114-122-84-120';
   static const String ngrokRegion = 'ap'; 
   
   // Server URLs
